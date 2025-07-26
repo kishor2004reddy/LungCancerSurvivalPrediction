15f3a7c35709c323cd0256c6ba55bd02cf87cf8f
Kishor Reddy <kishorreddy6407@gmail.com>
2025-07-26 12:24:26 +0530

    Added Procfile, render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from PodcastListeningTimePrediction import logger
 from flask import Flask, request, render_template, jsonify
 from PodcastListeningTimePrediction.pipeline.prediction_pipeline import PredictionPipeline
+import os
 
 app = Flask(__name__)
 
@@ -31,5 +32,6 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(port = 5050)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port)
 
